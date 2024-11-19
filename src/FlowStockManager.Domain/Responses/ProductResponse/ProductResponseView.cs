0a5d0253053b6253467871a24fa079bd1a9b2f7f
zGuian <guianmarcos32@gmail.com>
2024-11-19 16:12:24 -0300

    Ajustado request para aceitar array de objetos

--- src/FlowStockManager.Domain/Responses/ProductResponse/ProductResponseView.cs
+++ src/FlowStockManager.Domain/Responses/ProductResponse/ProductResponseView.cs
@@ -2,16 +2,16 @@
 {
     public class ProductResponseView<T> where T : class
     {
-        public T Object { get; private set; }
-
-        private ProductResponseView(T @object)
+        public IEnumerable<T> Object { get; private set; }
+        
+        private ProductResponseView(IEnumerable<T> @object)
         {
             Object = @object;
         }
 
         public static class Factories
         {
-            public static ProductResponseView<T> CreateResponseView(T @object)
+            public static ProductResponseView<T> CreateResponseView(IEnumerable<T> @object)
             {
                 return new ProductResponseView<T>(@object);
             }
