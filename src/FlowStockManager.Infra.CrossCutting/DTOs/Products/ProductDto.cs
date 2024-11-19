027f014ace48071ced54dbeba59a9641cbb718a5
zGuian <guianmarcos32@gmail.com>
2024-11-19 16:10:52 -0300

    Criado crosscutting para futuras configurações e ajustes no codigo.

---
+++ src/FlowStockManager.Infra.CrossCutting/DTOs/Products/ProductDto.cs
@@ -0,0 +1,4 @@
+﻿namespace FlowStockManager.Infra.CrossCutting.DTOs.Products
+{
+    public record ProductDto(Guid Id, string Name, string? Description, decimal Price, int StockQuantity, int MinimalStockQuantity);
+}
