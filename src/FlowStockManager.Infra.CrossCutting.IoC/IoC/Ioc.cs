807071a9b9018f777b9faa07e4df4dd195524142
zGuian <guianmarcos32@gmail.com>
2024-11-19 20:01:41 -0300

    • Adicionado controle de injeção de dependencia. • Adicionado configuração do AutoMapper.

---
+++ src/FlowStockManager.Infra.CrossCutting.IoC/IoC/Ioc.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using FlowStockManager.Application.Handlers;
+using FlowStockManager.Application.Handlers.Interfaces;
+using FlowStockManager.Application.Services;
+using FlowStockManager.Application.Services.Interfaces;
+using FlowStockManager.Application.UseCases;
+using FlowStockManager.Application.UseCases.Interfaces;
+using FlowStockManager.Domain.Interfaces;
+using FlowStockManager.Infra.CrossCutting.Profiles;
+using FlowStockManager.Infra.Data.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowStockManager.Infra.CrossCutting.IoC.IoC
+{
+    public static class Ioc
+    {
+        public static IServiceCollection IoC(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.DependencyInject();
+            return services;
+        }
+
+        private static IServiceCollection DependencyInject(this IServiceCollection services)
+        {
+            #region Handler
+            services.AddScoped<IProductHandler, ProductHandler>();
+            #endregion Handler
+
+            #region UseCases
+            services.AddScoped<IProductUseCase, ProductUseCase>();
+            #endregion UseCases
+
+            #region Services
+            services.AddScoped<IProductService,ProductService>();
+            services.AddScoped<ISupplierService, SupplierService>();
+            #endregion Services
+
+            #region Repositories
+            services.AddScoped<IProductRepository,ProductRepository>();
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+            #endregion Repositories
+
+            #region Database
+            services.AddSingleton(ConfigurationAutoMapper);
+            #endregion Database
+
+            #region Utils
+            services.AddSingleton(ConfigurationAutoMapper);
+            #endregion Utils
+
+            return services;
+        }
+
+        private static IMapper ConfigurationAutoMapper()
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new ProductMapper());
+            });
+            return mappingConfig.CreateMapper();
+        }
+    }
+}
---
+++ src/FlowStockManager.Infra.CrossCutting/Profiles/ProductMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FlowStockManager.Domain.Entities;
+using FlowStockManager.Domain.Requests.ProductRequests;
+using FlowStockManager.Infra.CrossCutting.DTOs.Products;
+
+namespace FlowStockManager.Infra.CrossCutting.Profiles
+{
+    public class ProductMapper : Profile
+    {
+        public ProductMapper()
+        {
+            CreateMap<Product, ProductDto>()
+                .ReverseMap();
+
+            CreateMap<Product, UpdateProductRequest>()
+                .ReverseMap();
+        }
+    }
+}
