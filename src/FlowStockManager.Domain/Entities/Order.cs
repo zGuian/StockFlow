0869cb7b15a0b9337c3ee12556e6071b1f7be37d
zGuian <guianmarcos32@gmail.com>
2024-12-09 14:55:33 -0300

    Adicionado regra para troca do pedido status

--- src/FlowStockManager.Domain/Entities/Order.cs
+++ src/FlowStockManager.Domain/Entities/Order.cs
@@ -24,11 +24,29 @@ namespace FlowStockManager.Domain.Entities
 
         public static Order UpdateOrderStatus(Order order, OrderStatus status)
         {
-            if (order.OrderStatus != OrderStatus.Pending)
+            switch (order.OrderStatus)
             {
-                throw new ErrorResponse("Status do pedido esta invalido");
+                case OrderStatus.Pending:
+                    if (status == OrderStatus.Processed || status == OrderStatus.Canceled)
+                    {
+                        order.OrderStatus = status;
+                    }
+                    break;
+                case OrderStatus.Processed:
+                    if (status == OrderStatus.Concluded || status == OrderStatus.Canceled)
+                    {
+                        order.OrderStatus = status;
+                    }
+                    break;
+                case OrderStatus.Concluded:
+                    order.OrderStatus = OrderStatus.Concluded;
+                    break;
+                case OrderStatus.Canceled:
+                    order.OrderStatus = OrderStatus.Canceled;
+                    break;
+                default:
+                    throw new ErrorResponse("Status do pedido esta invalido");
             }
-            order.OrderStatus = status;
             return order;
         }
 
