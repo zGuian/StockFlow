c7e8ed7316b793a0365b0d13c8a41bf2b904fc66
zGuian <Guianmarcos32@gmail.com>
2025-08-13 21:37:57 -0300

    [Feature]: Adicionado projeto desktop.

---
+++ src/FlowStockManager.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using FlowStockManager.Desktop.Models;
+using System.Windows;
+
+namespace FlowStockManager.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowModel();
+        }
+
+        [RelayCommand]
+        private void AbrirInicio()
+        {
+            // Trocar o conteúdo principal
+        }
+
+        [RelayCommand]
+        private void AbrirProdutos()
+        {
+            // Trocar para tela de produtos
+        }
+
+        [RelayCommand]
+        private void AbrirConfiguracoes()
+        {
+            // Trocar para tela de configurações
+        }
+    }
+}
---
+++ src/FlowStockManager.Desktop/Models/MainWindowModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FlowStockManager.Desktop.Models
+{
+    public partial class MainWindowModel : ObservableObject
+    {
+
+    }
+}
